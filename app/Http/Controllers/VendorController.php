d0eda7a3987e7e31d28f5623012f65e8d6671c43
dhiaa <dhiaadeveloper@gmail.com>
2023-11-27 15:56:39 +0300

    add use Image;

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+use Image;
 class VendorController extends Controller
 {
     public function VendorDashboard(){
